c17bbf72ca9e0e9129e47bbb7eee1f78c688bae8
Cesare Papa <papacesare05@gmail.com>
2023-11-14 08:44:09 +0100

    Aggiunta funzione per calcolare valore EAC e vari ETC
    1. aggiunta funzione calcoloEAT() che calcola l'EAT in base ai valori di
    ACWP e di ETC
    2. aggiunte funzioni calcoloETC() che calcola l'ETC in base ai valori
    dati nei vari modi possibili

--- CalcoloCosti/src/CalcoloCosti/calcolatoreCosti.java
+++ CalcoloCosti/src/CalcoloCosti/calcolatoreCosti.java
@@ -1,5 +1,31 @@
 package CalcoloCosti;
 
 public class calcolatoreCosti {
+	
+	public calcolatoreCosti() {};
 
+	
+	public float calcoloEAT(float ACWP, float ETC) {
+		float EAT = ACWP + ETC;	//calcolo l'EAT tramite i valori dati
+				
+		return EAT;	//ritorno EAT
+	}
+	
+	public float calcoloETC(float BAC, float BCWP) {
+		float ETC = BAC - BCWP;	//calcolo ETC in base ai valori dati
+		
+		return ETC;	//ritorno ETC
+	}
+	
+	public float calcoloETC(float BAC, float BCWP, float CPI) {
+		float ETC = (BAC - BCWP) / CPI;	//calcolo ETC in base ai valori dati
+		
+		return ETC;	//ritorno ETC
+	}
+	
+	public float calcoloETC(float BAC, float BCWP, float CPI, float SPI) {
+		float ETC = (BAC - BCWP) / (CPI * SPI);	//calcolo ETC in base ai valori dati
+		
+		return ETC;	//ritorno ETC
+	}
 }
