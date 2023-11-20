a8565a04087f5d63ac70146641c98b3721941c77
simon <simon@SimoneViotti>
2023-11-20 10:16:53 +0100

    controllo inserimento elementi dal utente e creazioni variabili utili

--- CalcoloCosti/src/CalcoloCosti/Avvio.java
+++ CalcoloCosti/src/CalcoloCosti/Avvio.java
@@ -13,3 +13,37 @@ public class Avvio {
 	}
 
 }
+
+package CalcoloCosti;
+
+public class Avvio {
+
+
+public static void main(String[] args) {
+
+
+if(args.length == 5) {
+
+int BAC = Integer.parseInt(args[0].toString());
+
+int performedActivities = Integer.parseInt(args[1].toString());
+
+int moneySpent = Integer.parseInt(args[2].toString());
+
+int expectedNumberOfPerformedActivities = Integer.parseInt(args[3].toString());
+
+int expectedMoneySpent = Integer.parseInt(args[4].toString());
+
+
+}
+
+else
+
+System.out.println("Usage: Avvio.java [BAC] + [Number of performed activites] + [Money spent] "
+
++ "+ [Expected number of performed activites] + [Expected money spent]");
+
+
+}
+
+}
