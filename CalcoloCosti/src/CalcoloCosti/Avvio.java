4e82e29ae734022e4c613d8e35125e125b785d5b
simon <simon@SimoneViotti>
2023-11-20 10:20:53 +0100

    sistemazione dei tab e rimozione parte vecchia di inizio progetto dopo controllo cesa

--- CalcoloCosti/src/CalcoloCosti/Avvio.java
+++ CalcoloCosti/src/CalcoloCosti/Avvio.java
@@ -1,49 +1,27 @@
 package CalcoloCosti;
 
 public class Avvio {
-	
-	public static void main(String argv) {
-		
-		if(argv.length() == 5) {
-			
-		}
-		else
-			System.out.println("Usage");
-		
-	}
-
-}
-
-package CalcoloCosti;
-
-public class Avvio {
 
 
 public static void main(String[] args) {
 
 
-if(args.length == 5) {
+	if(args.length == 5) {
 
-int BAC = Integer.parseInt(args[0].toString());
+		int BAC = Integer.parseInt(args[0].toString());
 
-int performedActivities = Integer.parseInt(args[1].toString());
+		int performedActivities = Integer.parseInt(args[1].toString());
 
-int moneySpent = Integer.parseInt(args[2].toString());
+		int moneySpent = Integer.parseInt(args[2].toString());
 
-int expectedNumberOfPerformedActivities = Integer.parseInt(args[3].toString());
+		int expectedNumberOfPerformedActivities = Integer.parseInt(args[3].toString());
 
-int expectedMoneySpent = Integer.parseInt(args[4].toString());
+		int expectedMoneySpent = Integer.parseInt(args[4].toString());
 
-
-}
-
-else
-
-System.out.println("Usage: Avvio.java [BAC] + [Number of performed activites] + [Money spent] "
-
-+ "+ [Expected number of performed activites] + [Expected money spent]");
-
-
-}
+	}
+	else {
+		System.out.println("Usage: Avvio.java [BAC] + [Number of performed activites] + [Money spent] "
+				+ "+ [Expected number of performed activites] + [Expected money spent]");
+	}
 
 }
